8f9630bebcbe48dcd56a4121b404a136995e14cb
b_sharpe <b_sharpe@2512258.no-reply.drupal.org>
2016-07-14 14:45:36 -0700

    Issue #2689477 by b_sharpe, mglaman: [Ubercart D6]  Write test for d6_ubercart_product

---
+++ tests/src/Kernel/d6/Ubercart/ProductTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\Tests\commerce_migrate\Kernel\d6\Ubercart;
+
+use Drupal\commerce_product\Entity\Product;
+
+/**
+ * Tests Product migration.
+ *
+ * @group commerce_migrate
+ */
+class ProductTest extends Ubercart6TestBase {
+
+  public static $modules = [
+    'text',
+    'action',
+    'system',
+    'entity',
+    'views',
+    'path',
+    'inline_entity_form',
+    'commerce_product',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->installEntitySchema('view');
+    $this->installEntitySchema('commerce_product_variation');
+    $this->installEntitySchema('commerce_product');
+    $this->createDefaultStore();
+    $this->executeMigrations([
+      'd6_ubercart_product_variation',
+      'd6_ubercart_product',
+    ]);
+  }
+
+  /**
+   * Test product migration from Drupal 6 to 8.
+   */
+  public function testProduct() {
+    $product = Product::load(1);
+    $this->assertNotNull($product);
+    $this->assertEquals('Product 27', $product->getTitle());
+    $this->assertEquals(FALSE, $product->isPublished());
+
+    /** @var \Drupal\commerce_product\Entity\ProductVariationInterface $variation */
+    $variation = $product->variations->first()->entity;
+    $this->assertNotEmpty($variation);
+    $this->assertEquals('MODEL-27', $variation->getSku());
+
+    $product = Product::load(3);
+    $this->assertNotNull($product);
+    $this->assertEquals('Product 29', $product->getTitle());
+    $this->assertEquals(1346271526, $product->getChangedTime());
+  }
+
+}
