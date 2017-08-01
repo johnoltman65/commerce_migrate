759ae35226992df874091c8da103800e470c326d
quietone <quietone@2572884.no-reply.drupal.org>
2017-08-01 07:40:38 -0600

    Issue #2894820 by quietone, heddn: [Commerce 1.x]  Migrate default currency

---
+++ modules/commerce/tests/src/Kernel/Migrate/d7/MigrateCurrencyTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\commerce_migrate_commerce\Kernel\Migrate\d7;
+
+use Drupal\Tests\commerce_migrate\Kernel\CommerceMigrateTestTrait;
+
+/**
+ * Tests currency migration.
+ *
+ * @group commerce_migrate
+ */
+class CurrencyTest extends Commerce1TestBase {
+
+  use CommerceMigrateTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->executeMigration('d7_currency');
+  }
+
+  /**
+   * Test currency migration from Drupal 7 to 8.
+   */
+  public function testCurrency() {
+    $this->assertCurrencyEntity('USD', 'USD', 'US Dollar', '840', 2, '$');
+  }
+
+}
