c46fd5cd74a2ff8b03f67719144263911f444bee
quietone <quietone@2572884.no-reply.drupal.org>
2018-04-03 09:31:24 -0600

    Issue #2905245 by quietone, heddn: Line item deriver

--- modules/commerce/src/Plugin/migrate/field/d7/CommercePrice.php
+++ modules/commerce/src/Plugin/migrate/field/d7/CommercePrice.php
@@ -4,9 +4,10 @@ namespace Drupal\commerce_migrate_commerce\Plugin\migrate\field\d7;
 
 use Drupal\migrate\Row;
 use Drupal\migrate_drupal\Plugin\migrate\field\FieldPluginBase;
+use Drupal\migrate\Plugin\MigrationInterface;
 
 /**
- * Field migration for commerce_price.
+ * Commerce price migrate field plugin.
  *
  * @MigrateField(
  *   id = "commerce_price",
@@ -22,6 +23,25 @@ class CommercePrice extends FieldPluginBase {
    */
   public function getFieldType(Row $row) {
     return 'commerce_price';
+  }
+
+    /**
+     * {@inheritdoc}
+     */
+  public function processFieldValues(MigrationInterface $migration, $field_name, $data) {
+    $destination_field_name = $field_name;
+    if ($field_name == 'commerce_unit_price') {
+      $destination_field_name = 'unit_price';
+    }
+    elseif ($field_name == 'commerce_total') {
+      $destination_field_name = 'total_price';
+    }
+    $process = [
+      'plugin' => 'commerce_migrate_commerce_price',
+      'source' => $field_name,
+    ];
+
+    $migration->setProcessOfProperty($destination_field_name, $process);
   }
 
 }
