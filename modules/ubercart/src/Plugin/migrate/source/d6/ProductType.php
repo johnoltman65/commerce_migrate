49342adc89a1cb5fe14c1b200c6b0a621e592378
quietone <quietone@2572884.no-reply.drupal.org>
2018-04-03 09:27:14 -0600

    Issue #2953581 by quietone, heddn: Extend ProductType source plugin from NodeType

--- modules/ubercart/src/Plugin/migrate/source/d6/ProductType.php
+++ modules/ubercart/src/Plugin/migrate/source/d6/ProductType.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\commerce_migrate_ubercart\Plugin\migrate\source\d6;
 
-use Drupal\migrate_drupal\Plugin\migrate\source\DrupalSqlBase;
+use Drupal\node\Plugin\migrate\source\d6\NodeType;
 
 /**
  * Gets Drupal 6 ubercart product classes from database.
@@ -15,37 +15,14 @@ use Drupal\migrate_drupal\Plugin\migrate\source\DrupalSqlBase;
  *   source_module = "uc_product"
  * )
  */
-class ProductType extends DrupalSqlBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function fields() {
-    return [
-      'type' => t('Type'),
-      'name' => t('Name'),
-      'description' => t('Description'),
-      'help' => t('Help'),
-      'revision' => t('Revision'),
-    ];
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getIds() {
-    $ids['type']['type'] = 'string';
-    $ids['type']['alias'] = 'nt';
-    return $ids;
-  }
+class ProductType extends NodeType {
 
   /**
    * {@inheritdoc}
    */
   public function query() {
-    $query = $this->select('node_type', 'nt')
-      ->fields('nt')
-      ->condition('module', 'uc_product%', 'LIKE');
+    $query = parent::query();
+    $query->condition('module', 'uc_product%', 'LIKE');
     return $query;
   }
 
