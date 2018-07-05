7666d3b9f77042b5a3ca56e0a2b7af6c53d65f82
quietone <quietone@2572884.no-reply.drupal.org>
2018-07-05 20:52:41 +1200

    Issue #2977978 by quietone: Add default store migration

---
+++ modules/ubercart/tests/src/Kernel/Migrate/uc6/DefaultStoreTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\Tests\commerce_migrate_ubercart\Kernel\Migrate\uc6;
+
+use Drupal\Tests\commerce_migrate\Kernel\CommerceMigrateTestTrait;
+
+/**
+ * Tests default store migration.
+ *
+ * @group commerce_migrate
+ * @group commerce_migrate_uc6
+ */
+class DefaultStoreTest extends Ubercart6TestBase {
+
+  use CommerceMigrateTestTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+    $this->migrateStore();
+    $this->executeMigration('uc6_default_store');
+  }
+
+  /**
+   * Test default store migration from Ubercart 6 to Commerce 2.
+   */
+  public function testMigrateDefaultStore() {
+    $this->assertDefaultStore();
+  }
+
+}
