d6b5bb6b83433e3f21096368d1925d940d3456e8
quietone <quietone@2572884.no-reply.drupal.org>
2019-01-17 08:29:57 +1300

    Issue #3025681 by quietone: UC6/UC7 Product and Product Variation source plugin test

--- modules/ubercart/src/Plugin/migrate/source/ProductVariationTypeTrait.php
+++ modules/ubercart/src/Plugin/migrate/source/ProductVariationTypeTrait.php
@@ -16,6 +16,8 @@ trait ProductVariationTypeTrait {
     // The dimensions and weights are relevant if commerce_shipping is enabled
     // on the destination.
     if ($this->getModuleHandler()->moduleExists('commerce_shipping')) {
+      $row->setSourceProperty('has_dimensions', FALSE);
+      $row->setSourceProperty('shippable', FALSE);
 
       // If any dimension column has non zero data then set has_dimensions true.
       $current_type = $row->getSourceProperty('type');
@@ -45,6 +47,20 @@ trait ProductVariationTypeTrait {
       }
     }
     parent::prepareRow($row);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fields() {
+    $fields = [];
+    if ($this->getModuleHandler()->moduleExists('commerce_shipping')) {
+      $fields = [
+        'has_dimensions' => $this->t('Set if this type has dimensions'),
+        'shippable' => $this->t('Set if this type is shippable'),
+      ];
+    }
+    return parent::fields() + $fields;
   }
 
 }
