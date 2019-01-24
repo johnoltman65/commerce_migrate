5617db4d52596270256e493425483a38bb208936
quietone <quietone@2572884.no-reply.drupal.org>
2019-01-25 06:18:38 +1300

    Issue #3028011 by quietone, heddn: Remove usage of deprecated TaxType source plugin

--- modules/ubercart/src/Plugin/migrate/source/uc6/TaxType.php
+++ modules/ubercart/src/Plugin/migrate/source/uc6/TaxType.php
@@ -4,8 +4,6 @@ namespace Drupal\commerce_migrate_ubercart\Plugin\migrate\source\uc6;
 
 use Drupal\commerce_migrate_ubercart\Plugin\migrate\source\TaxTypeBase;
 
-@trigger_error('TaxType is deprecated in Commerce Migrate 8.x-2.0-beta4 and will be removed before Commerce Migrate 8.x-3.x. Use \Drupal\commerce_migrate_ubercart\Plugin\migrate\source\TaxTypeBase instead.', E_USER_DEPRECATED);
-
 /**
  * Gets the Ubercart tax rates.
  *
@@ -13,8 +11,5 @@ use Drupal\commerce_migrate_ubercart\Plugin\migrate\source\TaxTypeBase;
  *   id = "uc6_tax_type",
  *   source_module = "uc_taxes"
  * )
- * @deprecated in Commerce Migrate 8.x-2.0-beta4, to be removed before Commerce
- * Migrate 8.x-3.x. Use
- * Drupal\commerce_migrate_ubercart\Plugin\migrate\source\TaxTypeBase instead.
  */
 class TaxType extends TaxTypeBase {}
